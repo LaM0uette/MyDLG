1a676ad8ddd89d359ce3f83c8888e22c0ce52bfd
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-31 19:01:06 +0200

    MyDlg page accueil - ui - cb

--- src/widgets/MyComboBox.py
+++ src/widgets/MyComboBox.py
@@ -46,5 +46,15 @@ class Base:
             dim=DcDim.Base(
                 fixed_width=PaDim.H4,
                 fixed_height=PaDim.H9,
+            ),
+
+            background=DcRgbBg.Base(
+                gen=PaRgb.TH3,
+                selection=PaRgb.TH3
+            ),
+
+            foreground=DcRgbFg.Base(
+                gen=PaRgb.TH1,
+                selection=PaRgb.TH1
             )
         )
