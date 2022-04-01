26128ca6c4f88035ff9ca3aa30b3ea992f022152
LaM0uette <lam0uette.dev@gmail.com>
2022-04-01 09:58:08 +0200

    sql ADD_ZONE_DLG

--- src/MyDLG.py
+++ src/MyDLG.py
@@ -298,7 +298,7 @@ class main(Ui_main, QtWidgets.QWidget):
             MsgBox.ALERTE(title="Erreur SQL", msg="Une erreur est survenue lors de la connexion à la base de données.")
             quit()
 
-        CoSql().ADD_ZONE_DLG(marche=20, nro=10, pm=5, refcode3='BITO')
+        CoSql().ADD_ZONE_DLG(marche=20, nro=10, pm=5, refcode3="BITO")
     def INIT(self, *args):
         for fct in args:
             splash_screen.lb_chargement.setText(fct[1])
