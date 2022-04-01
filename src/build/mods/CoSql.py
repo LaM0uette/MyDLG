a4b1a24b61688d5f218feebdb79661d6c6d2105e
LaM0uette <lam0uette.dev@gmail.com>
2022-04-01 11:04:01 +0200

    sql ADD_EXPORT

--- src/build/mods/CoSql.py
+++ src/build/mods/CoSql.py
@@ -11,6 +11,13 @@ class CoSql:
 
 
     def ADD_ZONE_DLG(self, marche, nro, pm, refcode3):
+        """
+        Insert une ligne dans la table t_zone_dlg s'il n'existe pas déjà avec les mêmes données.
+        :param marche: marché de la zone
+        :param nro: code NRO de la zone
+        :param pm: code PM de la zone
+        :param refcode3: refcode3 de la zone
+        """
         self.cd.execute(f"""
         INSERT INTO t_zone_dlg (zo_marche,zo_nro,zo_pm,zo_refcode3)
         SELECT {marche}, {nro}, {pm}, '{refcode3}'
@@ -25,6 +32,15 @@ class CoSql:
         self.close()
 
     def ADD_DLG(self, refcode3, date, phase, type_td, no_livraison, no_version):
+        """
+        Ajoute un dlg dans la table t_dlg s'il n'existe pas déjà avec les mêmes données.
+        :param refcode3: refcode3 de la zone
+        :param date: date de demande de traitement de l'export
+        :param phase: phase de l'export
+        :param type_td: transport uniquement T | distri uniquement D | les 2 TD
+        :param no_livraison: numéo de livraison
+        :param no_version: numéro de version de l'export
+        """
         self.cd.execute(f"""
         INSERT INTO t_dlg (dl_zo_id,dl_init_date,dl_phase,dl_td,dl_no_livraison,dl_no_version)
         SELECT (SELECT dlg.zo_id FROM t_zone_dlg dlg WHERE dlg.zo_refcode3 = '{refcode3}') AS zo, '{date}', '{phase}', '{type_td}', {no_livraison}, {no_version}
@@ -43,6 +59,12 @@ class CoSql:
         self.close()
 
     def ADD_EXPORT(self, dl_id, no_export, no_etat):
+        """
+        Ajoute un export dans la table t_exports s'il n'existe pas déjà avec les mêmes données.
+        :param dl_id: identifiant du dlg
+        :param no_export: numéro de l'export (nombre de fois qu'il a fallut le refaire)
+        :param no_etat: numéro qui correspond à l'état dans la table t_etats
+        """
         self.cd.execute(f"""
         INSERT INTO t_exports (ex_dl_id,ex_no_export,ex_date,ex_et_id)
         SELECT {dl_id}, {no_export}, '{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}', {no_etat};
