90c028c38e8842e887a50ffd17400b5ba71c60ba
LaM0uette <LaM0uette.dev@gmail.com>
2022-04-03 21:06:24 +0200

    ui des dlg

--- src/MyDLG.py
+++ src/MyDLG.py
@@ -419,6 +419,7 @@ class main(Ui_main, QtWidgets.QWidget):
         self.glay_dlg.addItem(spacerItem, i_init + 1, 0, 1, 1)
     def f_dlg_click(self, dlg_id, rgb, name_dlg):
         DlgBox.EDIT(dlg_id=dlg_id, rgb=rgb, name_dlg=name_dlg)
+        self.f_maj_dlg()
     #######################
     ##    /FONCTIONS     ##
     #######################
